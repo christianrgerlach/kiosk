e8c63c2a0651a5a549d7a71bac5d5c4ea8369a4d
Christian Gerlach <christianrgerlach@gmail.com>
2017-08-29 01:44:31 -0700

    found a bug
    cannot change active to false, tuple immutability issue

--- client_mon.py
+++ client_mon.py
@@ -7,13 +7,15 @@ from datetime import datetime
 client_list = {}
 
 def disconnect_monitor():
+	global client_list
+
 	while True:
 		for client in client_list:
 			time_since_checkin = (datetime.now() - client_list[client][2]).total_seconds()
 			#print 'checking client:', client, 'checked in', time_since_checkin, 'ago'
-			if time_since_checkin > 600:
+			if time_since_checkin > 120:
 				print client, 'lost!'
-				client_list[client][4] = False
+				client_list[client][3] = False
 		sleep(15)
 
 disconnect_monitor_daemon = Thread(target = disconnect_monitor)
